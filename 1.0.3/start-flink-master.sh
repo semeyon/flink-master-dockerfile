551403a6cb43cd216b1b4e7848a82ba2b7a769ee
smartpeople <semeyon@speople.pro>
2016-11-16 19:08:54 +0600

    FLINK_MASTER_IP moved to env file instead of auto detect

--- 1.0.3/start-flink-master.sh
+++ 1.0.3/start-flink-master.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 #Replace the FLINK_MASTER_PROPERTY=jobmanager.rpc.address with FLINK_MASTER_IP(default=127.0.0.1)
-FLINK_MASTER_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+# FLINK_MASTER_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 cat $FLINK_DIRECTORY/conf/flink-conf.yaml \
 | yaml-change $FLINK_MASTER_PROPERTY $FLINK_MASTER_IP > $FLINK_DIRECTORY/conf/flink-conf-new.yaml && \
 /bin/cp $FLINK_DIRECTORY/conf/flink-conf-new.yaml $FLINK_DIRECTORY/conf/flink-conf.yaml && \
