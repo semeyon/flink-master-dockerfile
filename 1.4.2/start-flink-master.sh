b2a64f65e26120f4c8b8b27badfc976f6eaaf370
smartpeople <semeyon@speople.pro>
2018-05-04 11:45:00 +0600

    Support for flink 1.4.2

---
+++ 1.4.2/start-flink-master.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -x
+#Replace the FLINK_MASTER_PROPERTY=jobmanager.rpc.address with FLINK_MASTER_IP(default=127.0.0.1)
+# FLINK_MASTER_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+cat $FLINK_DIRECTORY/conf/flink-conf.yaml \
+| yaml-change $FLINK_MASTER_PROPERTY $FLINK_MASTER_IP > $FLINK_DIRECTORY/conf/flink-conf-new.yaml && \
+/bin/cp $FLINK_DIRECTORY/conf/flink-conf-new.yaml $FLINK_DIRECTORY/conf/flink-conf.yaml && \
+rm $FLINK_DIRECTORY/conf/flink-conf-new.yaml
+
+#Increase akka.framesize parameter to run jobs using flink cli
+echo $AKKA_FRAMESIZE: $AKKA_FRAMESIZE_VAl >> $FLINK_DIRECTORY/conf/flink-conf.yaml
+
+# Start the web client
+$FLINK_DIRECTORY/bin/start-cluster.sh
+
+# Start the flink cluster
+# jobmanager.sh start cluster batch
+#$FLINK_DIRECTORY/bin/jobmanager.sh start cluster batch
+
+# Don't stop the container
+sleep infinity
